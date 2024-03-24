b08c79d6e3d5ce92a65ffaba0a219c1309d2d402
Shreyash0368 <shreyashgpt0503@gmail.com>
2024-03-24 16:43:38 +0530

    more setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import BloodTypeGrid from "./features/bloddUnits/BloodTypeGrid";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import BloodTypePage from "./features/bloddUnits/BloodTypePage";
+import DonorSignUp from "./features/donor/DonorSignup";
+import DonorLogin from "./features/donor/DonorLogin";
+import StaffLogin from "./features/staff/StaffLogin";
+
+const NotFound = () => {
+  // Display a "Page Not Found" message or handle it as needed
+  return <h1>404: Not Found</h1>;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route exact path="/" element={<BloodTypeGrid />}/>
+            <Route exact path="/bloodType/:type" element={<BloodTypePage />} />
+            <Route path='*' element={<Navigate to={'/'} />}/>
+            <Route exact path='/donorSignup' element={<DonorSignUp />} />
+            <Route exact path='/donorLogin' element={<DonorLogin />} />
+            <Route exact path='/staffLogin' element={<StaffLogin />} />
+          </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

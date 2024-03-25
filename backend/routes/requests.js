dd5776033afef28708384a4f5eda5efd3b3e2ab2
Shreyash0368 <shreyashgpt0503@gmail.com>
2024-03-25 17:14:25 +0530

    added adrequest route

--- backend/routes/requests.js
+++ backend/routes/requests.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Requests = require('../models/Requests')
+const fetchStaff = require('../middleware/fetchStaff')
 
-router.post('/addRequest', async (req, res) => {
-    const {blood_type, units, staff_id} = req.body;
+router.post('/addRequest',fetchStaff, async (req, res) => {
+    const staff_id = req.userid;
+    const role = req.role;
+
+    if (role !== 'staff') {
+        res.status(422).json({success: false, message: "Invalid Auth Token"}).send();  
+        return;
+    }
+    const {blood_type, units} = req.body;
 
     try {
         const request = new Requests({blood_type, units, staff_id});

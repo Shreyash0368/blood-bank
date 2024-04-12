b130bf64437a4149b785a39f191e176aef91064c
Shreyash0368 <shreyashgpt0503@gmail.com>
2024-04-12 19:59:45 +0530

    removing userdata from local storage upong closing of app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,30 @@
 import "./App.css";
+import React, {useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import BloodTypeGrid from "./features/bloddUnits/BloodTypeGrid";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import BloodTypePage from "./features/bloddUnits/BloodTypePage";
 import DonorSignUp from "./features/donor/DonorSignup";
 import DonorLogin from "./features/donor/DonorLogin";
 import StaffLogin from "./features/staff/StaffLogin";
 import ProfilePage from "./features/donor/ProfilePage";
+import StaffProfilePage from "./features/staff/StaffProfilePage";
 
 
-function App() {  
+function App() { 
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      localStorage.removeItem('role');  
+      localStorage.removeItem('userData');     
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []); 
+  
+
   return (
     <>
       <Router>
@@ -23,6 +37,7 @@ function App() {
             <Route exact path='/donorLogin' element={<DonorLogin />} />
             <Route exact path='/staffLogin' element={<StaffLogin />} />
             <Route exact path="/donor/profile/" element={<ProfilePage />}/>
+            <Route exact path="/staff/profile/" element={<StaffProfilePage />}/>
           </Routes>
       </Router>
     </>
